2dc4ea95d8f018ef696a386ca29acfabf1517275
Mahi <mahithegreat19@gmail.com>
2026-08-03 15:28:34 +0530

    shell for add

---
+++ client/src/components/AppShell.jsx
@@ -0,0 +1,29 @@
+
+
+
+export default function AppShell() {
+
+
+    return (
+        <div className="app-shell" >
+            {/* left sidebar */}
+            <aside className="sidebar glass-panel">
+                
+            </aside>
+
+            {/* Top bar */}
+            <header className="topbar">
+
+            </header>
+
+            <main className="canvas-area">
+
+            </main>
+
+            <aside className="right-panel glass-panel">
+
+            </aside>
+        </div>
+    )
+}
+
